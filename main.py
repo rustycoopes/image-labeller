a4af8347b9f9aa6fba1c8997fa72bfada622fd04
Russ Cooper <russell.cooper@btinternet.com>
2021-01-31 13:57:48 -0500

    print json

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def image_labeller_process_subscriber(request):
     print ("in function")
     request_json = request.get_json(silent=True)
     request_args = request.args
-
+    print(request_json)
     if request_json and 'name' in request_json:
         name = request_json['name']
     elif request_args and 'name' in request_args:
